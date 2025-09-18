43b578d89994e01919653f4172b817326f949252
Aravind C P <aravindcp@carestack.com>
2025-09-18 10:48:29 +0000

    Merged in revamp-cp (pull request #178)
    Revamp cp
    
    * sanity studio schema setup changes
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * data fetching methods added
    
    * who we server  page data fetch
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp

--- src/components/revamp/queries/index.ts
+++ src/components/revamp/queries/index.ts
@@ -1,22 +1,36 @@
 import groq from 'groq'
 import { getClient } from '~/lib/sanity.client'
 
+class Queries {
+  slug: string
+  client = getClient()
+  constructor(slug: string) {
+    this.slug = slug
+  }
 
- class Queries {
-    slug: string
-    client = getClient()
+  public fetchCommonData(slug: string) {
 
-    public fetchCommonData(slug: string) {
-        return groq`*[_type == "whoWeServe" && basicInfo.slug.current == "${this.slug}"][0]{
-           content {
-             sections[0]{
-              'tabs': component.tabsListingComponent.globalData->{
-                ...,
-              }
-             }
-           }
-         }`
-   }
- }
+    return groq`
+      *[_type == "whoWeServe" && basicInfo.slug.current == "${this.slug}"][0]{
+        content {
+          sections[0]{
+            'referencedTab': component.tabsListingComponent.globalData->{
+              ...,
+            },
+            'tabs': component.tabsListingComponent{
+              ...
+            }
+          }
+        }
+      }
+    `
+  }
 
- export default Queries
+  public async getData() {
+    const query = this.fetchCommonData(this.slug)
+    const params = { slug: this.slug }
+    return await this.client.fetch(query, params)
+  }
+}
+
+export default Queries
