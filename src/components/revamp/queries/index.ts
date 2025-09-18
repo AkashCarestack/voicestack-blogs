82754c539e3934af16ee5c99e308c5b9b6b4d81a
aravind886 <aravindcp@carestack.com>
2025-09-18 14:30:46 +0530

    data fetching methods added

---
+++ src/components/revamp/queries/index.ts
@@ -0,0 +1,22 @@
+import groq from 'groq'
+import { getClient } from '~/lib/sanity.client'
+
+
+ class Queries {
+    slug: string
+    client = getClient()
+
+    public fetchCommonData(slug: string) {
+        return groq`*[_type == "whoWeServe" && basicInfo.slug.current == "${this.slug}"][0]{
+           content {
+             sections[0]{
+              'tabs': component.tabsListingComponent.globalData->{
+                ...,
+              }
+             }
+           }
+         }`
+   }
+ }
+
+ export default Queries
