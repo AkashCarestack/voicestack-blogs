41e0450891948be2e5f04d52231b28d5b289245f
Aravind C P <aravindcp@carestack.com>
2025-09-23 11:08:41 +0000

    Merged in revamp-cp (pull request #185)
    Revamp cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * data fetching methods added
    
    * who we server  page data fetch
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * who we server  page data fetch
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * query optimized
    
    * revamp of voiceStack dev
    
    * Refactor dynamic components and queries to streamline TabsListing integration. Removed unused components and updated schema definitions for improved clarity and functionality.
    
    * voice stack 2.1 version  development
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * who we serve section
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * unwanted console removed
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Add FAQ Revamp Section and Update Related Components
    
    - Introduced a new FAQ section with dynamic content handling in `faqSection.tsx`.
    - Updated `sectionHeader.tsx` to include a mail link for support queries.
    - Modified `listingwithTabs.tsx` to enhance tab functionality with smooth scrolling on mobile.
    - Updated schema definitions to include `faqRevamp` in `sanity.config.ts` and related queries.
    - Adjusted `deskStructure.ts` to incorporate the new FAQ section for better content management.oic
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Enhance SectionHeader Component and Update TestShakir Page
    
    - Added a `className` prop to the `SectionHeaderProps` interface for better styling flexibility.
    - Updated the `SectionHeader` component to apply additional classes dynamically.
    - Modified the  page to include a background color in the main section.
    
    * Refactor Components to Enhance Layout and Functionality
    
    - Wrapped `ListingWithTabs` and `FaqSection` components in a `Container` for consistent padding and layout.
    - Updated `SwitchableTabs` to improve sticky positioning and background styling.
    - Adjusted  page to change background color and remove unnecessary `Container` wrapper.
    - Enhanced FAQ item rendering for better user interaction and visual consistency.
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp

--- src/pages/who-we-serve/test-shakir.tsx
+++ src/pages/who-we-serve/test-shakir.tsx
@@ -18,8 +18,8 @@ export default function TestShakir({ data }: TestShakirProps) {
     description: data?.content?.sections[0]?.data.subDescription,
   }
   return (
-    <Section className="flex flex-col bg-[#F4F3FA]">
-      <Container className="py-16 flex-col">
+    <Section className="flex flex-col bg-[#F9F9F9]">
+      {/* <Container className="py-16 flex-col"> */}
         <SectionHeader
         heading={headerData.heading}
         description={headerData.description}
@@ -33,7 +33,7 @@ export default function TestShakir({ data }: TestShakirProps) {
         </Button>
       </div>
       <FaqSection faqItems={data?.faq.faqItems} />
-      </Container>
+      {/* </Container> */}
 
     </Section>
   )
