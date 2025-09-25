2b9052700cb0235734a7577750314de319528767
Princy Saji <princysaji@carestack.com>
2025-09-25 17:53:56 +0530

    vertical slider style fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,18 @@ import { useTracking } from 'cs-tracker'
 import { isEmpty } from 'lodash'
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { useSearchParams } from 'next/navigation'
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
-import BannerSection from '~/components/BannerSection'
-import CardsListingSection from '~/components/CardsListingSection'
 import CustomHead from '~/components/common/CustomHead'
-import Header from '~/components/common/Header'
-import CsCardsListingSection from '~/components/CsCardsListingSection'
-import FaqSection from '~/components/revamp/components/common/components/faqSection'
-import FeatureSection from '~/components/features/FeatureSection'
-import LinksCardsSection from '~/components/LinksCardSection'
 import LogoListingSection from '~/components/LogoListingSection'
 import LogoSliderSection from '~/components/LogoSliderSection'
+import FaqSection from '~/components/revamp/components/common/components/faqSection'
+import HeroSection from '~/components/revamp/components/common/HeroSection/heroSection'
 import TablistSection from '~/components/revamp/components/common/TabListing/tablistingSection'
 import Testimonials from '~/components/revamp/components/common/Testimonials/Testimonials'
-import HeroSection from '~/components/revamp/components/common/HeroSection/heroSection'
+import VerticalTestimonialListing from '~/components/revamp/components/common/VerticalTestimonialListing/VerticalTestimonialListing'
 import Queries from '~/components/revamp/queries'
 import SiteComparisonSection from '~/components/SiteComparisonSection'
-import TestimonialHighlightSection from '~/components/TestimonialHighlightSection'
-import AnimatedBeamSection from '~/components/ui/animated/AnimatedBeamSection'
 import { getParams } from '~/helpers/getQueryParams'
 import { readToken } from '~/lib/sanity.api'
 import { getClient } from '~/lib/sanity.client'
@@ -38,12 +31,9 @@ import {
   getHeaderData,
   getIntegrationList,
   getTestimonialHighlightSectionData,
-  getVerticalTestimonialListing,
   logoSection,
 } from '~/lib/sanity.queries'
-import BookDemoContextProvider from '~/providers/BookDemoProvider'
 import runQuery from '~/utils/runQuery'
-import VerticalTestimonialListing from '~/components/revamp/components/common/VerticalTestimonialListing/VerticalTestimonialListing'
 
 export const getStaticProps: GetStaticProps<any> = async ({
   locale,
@@ -59,7 +49,7 @@ export const getStaticProps: GetStaticProps<any> = async ({
   const heroSectionData = await queries.getHeroData(region);
   const allTabsData = await queries.getAllTabsListingData(region)
   const testimonialSecitonData = allTabsData?.find(item => item.slug === 'testimonial-category-section')?.tabsListingComponent
-
+  const verticalTestimonialData = await queries.getVerticalTestimonialListing(region)
 
 // old queries
   const client = getClient(draftMode ? { token: readToken } : undefined)
@@ -78,7 +68,6 @@ export const getStaticProps: GetStaticProps<any> = async ({
   const testimonialHighlightsData = await getTestimonialHighlightSectionData(client,region)
   const bannerData = await getBannerData(client, region)
   const contactAndVideoData = await getContactAndVideoInfo(client, region)
-  const verticalTestimonialData = await getVerticalTestimonialListing(client, region)
 
   return {
     props: {
@@ -95,13 +84,13 @@ export const getStaticProps: GetStaticProps<any> = async ({
       logoSectionData,
       featureSectionData,
       testimonialSecitonData,
+      verticalTestimonialData,
       faqSectionData,
       cardsListingData,
       cSCardsListingData,
       testimonialHighlightsData,
       bannerData,
       contactAndVideoData,
-      verticalTestimonialData,
       tabListingData,
     },
     revalidate: 60
@@ -160,6 +149,7 @@ export default function IndexPage(
     homeSettings,
     heroSectionData,
     testimonialSecitonData,
+    verticalTestimonialData,
     logoSectionData,
     featureSectionData,
     integrationPlatforms,
@@ -171,7 +161,6 @@ export default function IndexPage(
     testimonialHighlightsData,
     bannerData,
     contactAndVideoData,
-    verticalTestimonialData,
     tabListingData,
   } = props
 
