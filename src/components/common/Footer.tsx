781fee11794ea668126dce19f489583e42888bd9
Akash as <akashas@carestack.com>
2025-10-06 16:44:01 +0530

    Update Footer Component for Responsive Text Styling
    - Adjusted the heading text size and styling in the Footer component to improve responsiveness across different screen sizes.
    - Ensured better visual consistency by modifying font sizes and leading for mobile and desktop views.

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = ({data}) => {
           {/* CTA Banner Section */}
           {data?.ctaBanner?.showBanner && data?.ctaBanner?.title && (
             <div className="  rounded-xl bg-[#4A3CE1] flex flex-col items-center gap-6 px-8 py-16 self-stretch">
-              <h3 className="text-center md:max-w-[818px] text-white font-manrope text-5xl font-bold leading-[60px] tracking-[-0.8px]">
+              <h3 className="text-center md:max-w-[818px] text-white font-manrope md:text-5xl text-xl font-bold md:leading-[60px] md:tracking-[-0.8px]">
                 {data.ctaBanner.title}
               </h3>
               {data.ctaBanner.buttonText && (
