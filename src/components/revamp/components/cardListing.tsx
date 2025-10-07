29285cf90ec6add19f27e8262f49970d67436fa8
Aravind C P <aravindcp@carestack.com>
2025-10-07 07:38:40 +0000

    Merged in revamp-cp (pull request #211)
    Revamp cp
    
    * folder name corrected to small case
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Add CardListing Component for Dynamic Tab Display
    
    - Introduced a new CardListing component to render tabbed content dynamically.
    - Integrated ImageLoader for displaying images with optimized loading.
    - Utilized SectionHeader for consistent heading and description presentation.
    - Implemented responsive grid layout for tab items, enhancing user experience.
    
    * Add CardListing Component for Dynamic Tab Display
    
    - Introduced a new CardListing component to render tabbed content dynamically.
    - Integrated ImageLoader for image handling and utilized urlForImage for image URLs.
    - Implemented a responsive layout with SectionHeader and Container for better presentation of tab data.
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * code aligned
    
    * Enhance FAQ Section with Category Navigation and Dynamic Question Display
    
    - Refactored FaqSection component to support category-based navigation for FAQs.
    - Implemented state management for active categories and question toggling.
    - Updated schema to define FAQ categories and their associated questions.
    - Modified data fetching logic to retrieve FAQ data from the new structure.
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branches 'revamp-cp' and 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * transition effect for faq added
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * faq added
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Add hideCategory feature to FAQ section
    
    - Introduced a new boolean field 'hideCategory' in the FAQ schema to control the visibility of the categories.
    - Updated the FAQ section component to conditionally render the categories based on the 'hideCategory' state.
    - Modified data fetching logic to include 'hideCategory' in the FAQ data retrieval.
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Merge branch 'revamp' of https://bitbucket.org/cstack-bb/voicestack-sanity into revamp-cp
    
    * Fix import casing for WhoWeServe and WhyVoicestack components in index.ts

--- src/components/revamp/components/cardListing.tsx
+++ src/components/revamp/components/cardListing.tsx
@@ -39,10 +39,11 @@ export default function CardListing({ data }: any) {
                       className="w-full h-full object-cover md:rounded-[24px] rounded-[12px] overflow-hidden"
                     />
                   </div>
-                  <div className="flex flex-col gap-3 md:py-8 py-4 md:px-3 px-2">
+                  <div className="flex flex-col  md:py-8 py-4 md:px-3 px-2">
                     <div className="text-sm  font-normal text-gray-950 uppercase opacity-70">
                       {e.tabHeading}
                     </div>
+                    <div className='flex flex-col gap-3'>
                     <div className="md:text-2xl text-xl font-bold text-gray-950 leading-[133.33%] font-manrope">
                       {e.tabSubHeading}
                     </div>
@@ -59,6 +60,8 @@ export default function CardListing({ data }: any) {
                         </p>
                       )
                     )}
+                    </div>
+                  
                   </div>
                 </div>
               )
