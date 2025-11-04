4c94fa4aaa3931d993359596fbb16d476ad3ba28
aravind886 <aravindcp@carestack.com>
2025-11-04 12:05:38 +0530

    build error fixes

--- src/pages/ai-receptionist.tsx
+++ src/pages/ai-receptionist.tsx
@@ -82,16 +82,16 @@ export default function AiReceptionist({
 
             {/* <CategoryFeatureTabs features={features} /> */}
             {
-                pageData['real-business-outcomes']?.componentData && (
+                pageData['real-business-outcomes']?.componentData?.refData?.tabsListingComponent && (
                     <CardListing 
-                        data={pageData["real-business-outcomes"].componentData.refData.tabsListingComponent}
+                        data={pageData["real-business-outcomes"]?.componentData?.refData?.tabsListingComponent}
                     />
                 )
             }
 
             {pageData['how-voicestack-works']?.componentData && (
                 <CardsGridSection
-                    data={pageData['how-voicestack-works'].componentData}
+                    data={pageData['how-voicestack-works']?.componentData}
                 />
             )}
 
