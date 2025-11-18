a0e07fbd0659c3eccae91dc21d21666b5f2bfca9
aravind886 <aravindcp@carestack.com>
2025-11-18 14:00:15 +0530

    cosmetic fixes

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const config = {
+  compiler:{
+    removeConsole:process.env.NODE_ENV =='production'
+  },
   turbopack:{
     root: path.resolve(__dirname),
   },
