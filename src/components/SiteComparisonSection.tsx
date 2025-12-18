9a41bcef74d80a5dd9a5a81887cdf7c30f69b226
shakir <shakirm@carestack.com>
2025-12-18 17:03:28 +0530

    compponents added to the au home page

--- src/components/SiteComparisonSection.tsx
+++ src/components/SiteComparisonSection.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router'
 import LegendSection from './common/LegendSection'
 import ComparisonTable from './ComparisonTable'
 import SectionHeader from './revamp/components/common/sectionHeader'
+import SectionHeaderV2 from './revamp/components/common/sectionHeaderV2'
 
 function SiteComparisonSection({ data, legendData, refer=null, variant }) {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
@@ -30,8 +31,39 @@ function SiteComparisonSection({ data, legendData, refer=null, variant }) {
   
   return (
     hideTable ? (
-      <></> 
+      <></>
     ):(
+      variant === 'V2' ? (
+        <Section className='bg-[#ffffff]' border="b">
+        <Container className='w-full py-sm md:py-md lg:py-lg' type="V2" border="y-0">
+          <div className="flex-col relative w-full flex gap-16">
+            <SectionHeaderV2 className='xl:px-12 md:px-6 px-4'
+              heading={"Best phone system.<br> For the best dental practices."}
+              description={"No other phone system can match VoiceStack’s AI-driven features,outcome-driven workflows and integration capabilities, as shown in the comparison chart below."}
+            />
+
+          <div className='flex flex-col items-center w-full mt-12'>
+            <div className='w-full flex flex-col gap-2 '>
+              <ComparisonTable
+                variant={variant}
+                data={{
+                  columnDimensionName: data.columnDimensionName,
+                  ...data.table,
+                }}
+                legendData={legendData}
+                demoLink={null}
+              />
+            </div>
+            {/* {isMobile ? <LegendSection/> :
+              <div className="flex justify-end pt-6 w-full">
+              <span className='text-[11px] md:text-sm text-gray-600'>*Data from 3rd Party Services.</span>
+              </div>
+            } */}
+          </div>
+          </div>
+        </Container>
+      </Section>
+      ) : (
       <Section id="comparison" className="py-sm md:py-md lg:py-lg scroll-m-16 bg-[#F9F9F9]">
         <Container className="flex flex-col items-center gap-16">
           <SectionHeader
@@ -40,18 +72,15 @@ function SiteComparisonSection({ data, legendData, refer=null, variant }) {
           />
           
           <div className='flex flex-col gap-12 items-center w-full'>
-           
-  
             <div className='w-full flex flex-col gap-2 '>
-              
               <ComparisonTable
-              data={{
-                columnDimensionName: data.columnDimensionName,
-                ...data.table,
-              }}
-              legendData={legendData}
-              demoLink={null}
-            />
+                data={{
+                  columnDimensionName: data.columnDimensionName,
+                  ...data.table,
+                }}
+                legendData={legendData}
+                demoLink={null}
+              />
             </div>
             {/* {isMobile ? <LegendSection/> :
               <div className="flex justify-end pt-6 w-full">
@@ -68,6 +97,7 @@ function SiteComparisonSection({ data, legendData, refer=null, variant }) {
             </div>
         </Container>
       </Section>
+      )
     )
 
   )
