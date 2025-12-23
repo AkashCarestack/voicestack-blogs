d131c668a322fa4709ba10468352f634aac4c029
Akash as <akashas@carestack.com>
2025-12-23 15:29:56 +0530

    Add LinkedIn Insight Tag for enhanced analytics tracking
    - Integrated LinkedIn Insight Tag scripts into the _app.tsx file to enable tracking of user interactions and improve analytics capabilities.
    - Included noscript fallback for users with JavaScript disabled, ensuring data collection remains effective.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -156,6 +156,37 @@ function App({
           />
         </noscript>
 
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight-partner-id" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "8476516";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-insight-loader" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8476516&fmt=gif"
+          />
+        </noscript>
+
         {/* <!--[BEGIN Google Tag Manager (noscript)]--> */}
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCX7H59S" height="0" width="0"
           style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
