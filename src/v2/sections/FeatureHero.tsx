aca75206ab71203d5f6a43b21d103a35122fa486
aravind886 <aravindcp@carestack.com>
2026-01-07 19:35:39 +0530

    Refactor Comparison and Partner Pages
    - Removed console log from PartnerSlugPage for cleaner code.
    - Enhanced ComparisonSlugPage by integrating new sections: OfferSection and IntegrationsShowcaseSection for improved content display.
    - Updated FeatureHero component to support a new 'form' type and added a hideBg prop for better background control.
    - Adjusted getStaticProps to fetch features list, enhancing data handling for the Comparison page.

--- src/v2/sections/FeatureHero.tsx
+++ src/v2/sections/FeatureHero.tsx
@@ -10,7 +10,13 @@ import Section from '~/components/structure/Section'
 import HubspotGenericForm from '~/components/revamp/components/common/hubspotGeneric'
 import LightningIcon from '../icons/LightningIcon'
 
-export default function FeatureHero({ data, type }: { data: any, type?: string }) {
+interface FeatureHeroProps {
+  data: any
+  type?:string | 'form' | 'feature'
+  hideBg?: boolean
+}
+
+export default function FeatureHero({ data, type , hideBg = false}: { data: any, type?: string, hideBg?: boolean }) {
   console.log(data, 'data');
   const hubspotFormId = data?.componentData?.hubspotFormId || data?.hubspotFormId
   const value = data?.heroComponent 
@@ -123,8 +129,8 @@ export default function FeatureHero({ data, type }: { data: any, type?: string }
     <Section className="relative overflow-hidden" id="FeatureHero" border="b">
       <Container type="V2" className="md:py-24 py-16 overflow-hidden justify-center flex">
         <div className='flex lg:flex-row flex-col md:gap-12  max-w-[1240px] w-full gap-6 relative z-10 items-center'>
-          <div className={`flex flex-col gap-3 relative z-10 flex-1  ${type === 'partner' ? 'max-w-[606px]' : ''}`}>
-            {(type === 'partner' || type === 'comparison') ? (
+          <div className={`flex flex-col gap-3 relative z-10 flex-1  ${type === 'form' ? 'max-w-[606px]' : ''}`}>
+            {(type === 'form' || type === 'comparison') ? (
               <div className="flex items-center gap-2 py-[9px] pr-4 pl-[14px] rounded-full border border-[#AEA0FF] lg:self-start self-center bg-white/20 shadow-[-7px_0_10px_0_rgba(251,111,142,0.5),7px_0_10px_0_rgba(74,60,225,0.5)]">
                 <LightningIcon className="w-4 h-4" />
                 <h2 className="text-center md:text-left text-sm font-geist font-normal leading-[115%] text-gray-950">
@@ -138,7 +144,7 @@ export default function FeatureHero({ data, type }: { data: any, type?: string }
             )}
             <PortableText
               value={heading}
-              components={type === 'feature' || type === 'partner' ? HeroFeatureComponents : type === 'comparison' ? ComparisonHeroH1 : HeroFeatureHeadingComponents}
+              components={type === 'feature' ? HeroFeatureComponents : type === 'form' ? ComparisonHeroH1 : HeroFeatureHeadingComponents}
             />
             <PortableText
               value={description}
@@ -394,6 +400,7 @@ export default function FeatureHero({ data, type }: { data: any, type?: string }
             </div>
           )}
         </div>
+        {!hideBg &&
         <div className="hidden z-0 md:block absolute right-0 bottom-0 w-[1000px] h-[738px] pointer-events-none">
           <Image
             className="w-full h-full object-cover"
@@ -403,6 +410,7 @@ export default function FeatureHero({ data, type }: { data: any, type?: string }
             src={bgStyle.src}
           />
         </div>
+    }
       </Container>
     </Section>
   )
