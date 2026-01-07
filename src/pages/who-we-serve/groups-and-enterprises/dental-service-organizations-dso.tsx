f52bb9ff79a14a03d92b6b0c49714bbfd0d73694
aravind886 <aravindcp@carestack.com>
2026-01-07 12:53:30 +0530

    dental-service-organizations-dso page added

--- src/pages/who-we-serve/groups-and-enterprises/dental-service-organizations-dso.tsx
+++ src/pages/who-we-serve/groups-and-enterprises/dental-service-organizations-dso.tsx
@@ -1,11 +1,13 @@
 import { GetStaticProps } from 'next'
 import React from 'react'
-import LogoListingV2 from '~/components/LogoListingV2'
+import StackCardTestimonial from '~/components/revamp/components/common/stackCardTestimonial/stackCardTestimonial'
 import StatisticsSection from '~/components/revamp/components/StatisticsSection'
 import Queries from '~/components/revamp/queries'
-import Section from '~/components/structure/Section'
+import CategoryFeatureTabsSection from '~/v2/sections/CategoryFeatureTabsSection'
 import FeatureHero from '~/v2/sections/FeatureHero'
-import GroupedCardsGridSection from '~/v2/sections/GroupedCardsGridSection'
+import IntegrationsShowcaseSection from '~/v2/sections/IntegrationsShowcaseSection'
+import LogoListingV2 from '~/v2/sections/LogoListingV2'
+import VerticalTestimonialListing from '~/v2/sections/verticalTestimonialSection'
 
 interface DentalServiceOrganizationsDSOProps {
   pageData: any
@@ -18,25 +20,62 @@ export default function DentalServiceOrganizationsDSO({
   faq,
   region,
 }: DentalServiceOrganizationsDSOProps) {
+  console.log({ pageData }, 'pageData')
   return (
     <>
-    <FeatureHero
-      data={pageData['dso-hero']?.componentData}
-      type="feature"
-    />
-    <div>
-      {pageData['logo-listing']?.componentData && (
-        <LogoListingV2
-          data={pageData['logo-listing']?.componentData.blocksListingData}
-        />
-      )}
-          {pageData['card-with-image'] && (
+      <FeatureHero data={pageData['dso-hero']?.componentData} type="feature" />
+      <div>
+        {pageData['logo-listing']?.componentData && (
+          <LogoListingV2
+            data={pageData['logo-listing']?.componentData.blocksListingData}
+          />
+        )}
+        {pageData['testimonial-video-section']?.componentData?.refData
+          ?.testimonialListing && (
+          <VerticalTestimonialListing
+            data={
+              pageData['testimonial-video-section']?.componentData?.refData
+                ?.testimonialListing
+            }
+          />
+        )}
+        {pageData['stack-card-tab-testimonial']?.componentData?.refData ? (
+          <StackCardTestimonial
+            data={
+              pageData['stack-card-tab-testimonial']?.componentData?.refData
+                ?.tabsListingComponent
+            }
+          />
+        ) : (
+          <StackCardTestimonial
+            data={pageData['stack-card-tab-testimonial']?.componentData}
+          />
+        )}
+        {/* {pageData['card-with-image'] && (
         <GroupedCardsGridSection
           data={pageData['card-with-image']?.genericListingComponent}
         />
-      )}
-       <StatisticsSection variant="V2" />
-    </div>
+      )} */}
+        <StatisticsSection variant="V2" />
+        {pageData['integrations-listing']?.componentData && (
+          <IntegrationsShowcaseSection
+            data={pageData['integrations-listing']?.componentData}
+            theme="dark"
+          />
+        )}
+
+        <CategoryFeatureTabsSection
+          features={
+            pageData['manage-every-calls']?.componentData?.refData
+              ?.tabsListingComponent
+          }
+          variant="scrollcarousel"
+          sectionHeading={
+            pageData['manage-every-calls']?.componentData?.refData
+              ?.tabsListingComponent
+          }
+        />
+      </div>
     </>
   )
 }
