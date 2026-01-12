56508e4fd8409bb8d6c50d34da9d259f5d0f3378
aravind886 <aravindcp@carestack.com>
2026-01-12 12:59:51 +0530

    Remove CustomHead and GlobalHead components; enhance SEO in SimpleHead and Header components with additional meta tags for improved indexing and social sharing. Update schema to include disableIndex field for better content management.

--- src/components/common/SimpleHead.tsx
+++ src/components/common/SimpleHead.tsx
@@ -14,7 +14,7 @@ export default function SimpleHead({ data, noindex = false }: SimpleHeadProps) {
   const { alternatePaths, defaultUrl } = useAlternatePaths();
 
   const fullTitle = data?.metaTitle ? `${data?.metaTitle}` : 'VoiceStack® | AI Powered Enterprise Phone System';
-
+  console.log("data", data);
   return (
     <Head>
       <title>{fullTitle}</title>
@@ -24,9 +24,24 @@ export default function SimpleHead({ data, noindex = false }: SimpleHeadProps) {
       {data?.canonical && <link rel="canonical" href={data?.canonical} />}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={fullTitle} />
+      <meta property="og:url" content={data?.canonical} />
       <meta property="og:description" content={data?.metaDescription || 'AI Powered Enterprise Phone System'} />
       <meta name="title" content={fullTitle} />
-      {noindex && <meta name="robots" content="noindex, nofollow, noarchive" />}
+      <meta name="robots" content={data?.disableIndex ? 'noindex, nofollow, noarchive' : 'index, follow'} />
+
+
+      {/* twitter tags starts here */}
+
+
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:domain" content="voicestack-sanity-gamma.vercel.app"/>
+      {data?.canonical && <meta property="twitter:url" content={data?.canonical}/>}
+      {fullTitle && <meta name="twitter:title" content={fullTitle}/>} 
+      {data?.metaDescription && <meta name="twitter:description" content={data?.metaDescription}/>}
+      {noindex && <meta name="robots" content="noindex, nofollow, noarchive" /> }
+
+
+      
       
       {alternatePaths.length > 0 && alternatePaths.map((item: AlternatePath) => (
         <link 
