d3f1b2930f79b0a0c8fed34c9bd1f43d1518ab5c
Akash as <akashas@carestack.com>
2026-01-19 13:12:02 +0530

    Update StatisticsSection to use a static heading for improved clarity. Commented out the previous rotating word heading for future reference.

--- src/v2/sections/StatisticsSection.tsx
+++ src/v2/sections/StatisticsSection.tsx
@@ -43,7 +43,9 @@ const StatisticsSection = ({ bgColor }: StatisticsSectionProps = {}) => {
             <div className="flex-col relative w-full flex gap-8">
 
               <SectionHeaderV2 isLeftAlign={true} className=''
-                heading={createRotatingWordHeading("The Most Advanced AI Phone System for Practices Globally.")}
+                heading={"The Most Advanced AI Phone System for Practices Globally."}
+                // heading={createRotatingWordHeading("The Most Advanced AI Phone System for Practices Globally.")}
+
                 // heading={pageData['how-voicestack-works2'].componentData.heading}
                 description={"VoiceStack is the fastest-growing AI phone system preferred by growth-focused practices across the US, UK, and Australia. From single offices to practices with hundreds of locations, brands rely upon VoiceStack's superior AI models, guaranteed reliability, and ease of use to create delightful patient experiences and sustained growth."}
               />
