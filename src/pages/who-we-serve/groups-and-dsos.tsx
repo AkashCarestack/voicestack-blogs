cd786714d46f6003ac14476c75ea49829d8b8753
shakir <shakirm@carestack.com>
2026-01-20 13:48:33 +0530

    dsos page added

--- src/pages/who-we-serve/groups-and-dsos.tsx
+++ src/pages/who-we-serve/groups-and-dsos.tsx
@@ -10,8 +10,14 @@ import StackCardTestimonial from '~/components/revamp/components/common/stackCar
 import SingleTabCardListing from '~/components/revamp/components/common/TabListing/singleTabCardListing'
 import TabCardsListing from '~/components/revamp/components/common/TabListing/tabCardsListing'
 import VerticalTestimonialListing from '~/components/revamp/components/common/VerticalTestimonialListing/VerticalTestimonialListing'
-import StatisticsSection from '~/components/revamp/components/StatisticsSection'
 import Queries from '~/components/revamp/queries'
+import CategoryFeatureTabsSection from '~/v2/sections/CategoryFeatureTabsSection'
+import FeatureHero from '~/v2/sections/FeatureHero'
+import IntegrationsShowcaseSection from '~/v2/sections/IntegrationsShowcaseSection'
+import LogoListingV2 from '~/v2/sections/LogoListingV2'
+import StatisticsSection from '~/v2/sections/StatisticsSection'
+import SwitchableTabsV2 from '~/v2/sections/SwitchableTabsV2'
+import VerticalTestimonialListingv2 from '~/v2/sections/verticalTestimonialSection'
 
 interface GroupsAndDSOProps {
   pageData: any
@@ -22,23 +28,79 @@ export default function GroupsAndDSO({ pageData, faq }: GroupsAndDSOProps) {
   const tabsListingData = pageData?.["mobile-practices"]?.componentData?.refData?.tabsListingComponent
   const tabsListingComponentData =
     pageData['smarter-systems']?.componentData?.refData?.tabsListingComponent
+  // return (
+  //   <>
+  //     <SimpleHead data={pageData?.seo} />
+      
+  //     <HeroWrapper>
+  //       <Breadcrumb breadCrumb={pageData?.breadCrumb} />
+  //       <HeroSection
+  //         page=""
+  //         data={pageData['dental-phones-hero']?.componentData}
+  //       />
+  //     </HeroWrapper>
+
+  //     {/* {tabsListingComponentData && (
+  //       <SingleTabCardListing data={tabsListingComponentData} />
+  //     )} */}
+  //     {pageData['testimonial-video-section']?.componentData && (
+  //       <VerticalTestimonialListing
+  //         data={
+  //           pageData['testimonial-video-section']?.componentData?.refData
+  //             ?.testimonialListing
+  //         }
+  //       />
+  //     )}
+  //     <StatisticsSection />
+  //     {pageData['stack-card-tab-testimonial']?.componentData?.refData ? (
+  //       <StackCardTestimonial
+  //         data={
+  //           pageData['stack-card-tab-testimonial']?.componentData?.refData
+  //             ?.tabsListingComponent
+  //         }
+  //       />
+  //     ) : (
+  //       <StackCardTestimonial
+  //         data={pageData['stack-card-tab-testimonial']?.componentData}
+  //       />
+  //     )}
+  //     {pageData['integrations-listing']?.componentData && (
+  //       <div className="mt-12">
+  //         <IntegrationsGrid
+  //           data={pageData['integrations-listing']?.componentData}
+  //         />
+  //       </div>
+  //     )}
+
+  //     {
+  //       tabsListingData && (<TabCardsListing data={tabsListingData} />)
+  //     }
+
+  //     {/* FAQ Section */}
+  //     {faq && (
+  //       <div>
+  //         <FaqSection faqItems={faq} />
+  //       </div>
+  //     )}
+  //   </>
+  // )
   return (
     <>
-      <SimpleHead data={pageData?.seo} />
-      
-      <HeroWrapper>
-        <Breadcrumb breadCrumb={pageData?.breadCrumb} />
-        <HeroSection
-          page=""
-          data={pageData['dental-phones-hero']?.componentData}
+      <Breadcrumb breadCrumb={pageData?.breadCrumb} />
+      {pageData['mobile-practices-hero']?.componentData && (
+        <FeatureHero data={pageData['mobile-practices-hero']} type="feature" />
+      )}
+      {pageData['logos-listing']?.componentData && (
+        <LogoListingV2
+          data={pageData['logos-listing']?.componentData.blocksListingData}
         />
-      </HeroWrapper>
-
-      {/* {tabsListingComponentData && (
-        <SingleTabCardListing data={tabsListingComponentData} />
-      )} */}
-      {pageData['testimonial-video-section']?.componentData && (
-        <VerticalTestimonialListing
+      )}
+      {tabsListingComponentData && (
+        <SwitchableTabsV2 data={tabsListingComponentData} />
+      )}
+      {pageData['testimonial-video-section']?.componentData?.refData
+        ?.testimonialListing && (
+        <VerticalTestimonialListingv2
           data={
             pageData['testimonial-video-section']?.componentData?.refData
               ?.testimonialListing
@@ -46,36 +108,22 @@ export default function GroupsAndDSO({ pageData, faq }: GroupsAndDSOProps) {
         />
       )}
       <StatisticsSection />
-      {pageData['stack-card-tab-testimonial']?.componentData?.refData ? (
-        <StackCardTestimonial
-          data={
-            pageData['stack-card-tab-testimonial']?.componentData?.refData
-              ?.tabsListingComponent
-          }
-        />
-      ) : (
-        <StackCardTestimonial
-          data={pageData['stack-card-tab-testimonial']?.componentData}
-        />
-      )}
       {pageData['integrations-listing']?.componentData && (
-        <div className="mt-12">
-          <IntegrationsGrid
-            data={pageData['integrations-listing']?.componentData}
-          />
-        </div>
+        <IntegrationsShowcaseSection
+          data={pageData['integrations-listing']?.componentData}
+          theme="dark"
+        />
       )}
-
-      {
-        tabsListingData && (<TabCardsListing data={tabsListingData} />)
-      }
-
-      {/* FAQ Section */}
-      {faq && (
-        <div>
-          <FaqSection faqItems={faq} />
-        </div>
-      )}
+       <CategoryFeatureTabsSection
+        features={
+          pageData['manage-every-calls']?.componentData?.refData
+            ?.tabsListingComponent
+        }
+        variant="scrollcarousel"
+        sectionHeading={
+          pageData['manage-every-calls']?.componentData?.refData?.tabsListingComponent
+        }
+      />
     </>
   )
 }
@@ -93,6 +141,9 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 
     if (!pageData) {
       console.error(`pageData not found for ${slug}`)
+      return {
+        notFound: true
+      }
     }
     const faqData =
       pageData?.faqData?.[0] || pageData?.faqReferenced?.[0] || null
