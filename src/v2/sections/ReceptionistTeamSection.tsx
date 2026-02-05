c1622340a12d14f0d1526ae4c28021c0a291fcd6
aravind886 <aravindcp@carestack.com>
2026-02-05 15:17:22 +0530

    fix

--- src/v2/sections/ReceptionistTeamSection.tsx
+++ src/v2/sections/ReceptionistTeamSection.tsx
@@ -145,11 +145,11 @@ export default function ReceptionistTeamSection({ data }: ReceptionistTeamSectio
     // Slider settings for 3 items with arrows and no dots
     const settings = {
         dots: false,
-        infinite: displayItems.length > 3,
+        infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2000,
         autoplay: true,
         arrows: true,
         cssEase: "linear",
