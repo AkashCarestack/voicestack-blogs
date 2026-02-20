693613c61eb687b35982391210acdc0fc80a6afb
shakir <shakirm@carestack.com>
2026-02-20 16:13:37 +0530

    qa fixes

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -347,7 +347,7 @@ const Header = ({ data, refer = null }) => {
         <link rel="alternate" hrefLang="en-au" href="https://www.voicestack.com/en-AU" />
         <link rel="alternate" hrefLang="x-default" href="https://www.voicestack.com" /> */}
         {/* organization schema */}
-        {OrganizationSchemaData && (
+        {OrganizationSchemaData && router.locale !== 'en-GB' && (
           <>
           <meta name="author" content="VoiceStack®"></meta>
           <meta property="og:image" content={urlForImage(siteSettings?.ogImage)} />
