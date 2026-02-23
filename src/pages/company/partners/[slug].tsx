647240c1f3bbd52b299c8ba0b9977bd5692ca897
shakir <shakirm@carestack.com>
2026-02-23 17:39:04 +0530

    bug fix

--- src/pages/company/partners/[slug].tsx
+++ src/pages/company/partners/[slug].tsx
@@ -82,6 +82,7 @@ export default function PartnerSlugPage({
 
       {features && features.length > 0 && pageData['category-feature-tabs'] && (
         <CategoryFeatureTabsSection
+        
           features={features}
           variant="carousel"
           sectionHeading={
@@ -117,7 +118,7 @@ export default function PartnerSlugPage({
         />
       )}
 
-      <StatisticsSection category="Veterinarians"/>
+      <StatisticsSection {...(slug === 'vetcelerator' && { category: 'Veterinarians' })} />
     </>
   )
 }
